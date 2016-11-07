5f81bfcc4b0b339fd7fd12c33c269897d6bf6e80
bestrandomnameever <bestrandomnameever@gmail.com>
2016-11-07 14:00:31 +0100

    Tutorial gevolgd tot H5
    Alles van Introduction - Interacting with a server using an ES6 service

---
+++ src/js/services/user.service.js
@@ -0,0 +1,27 @@
+export default class User {
+  constructor(AppConstants, $http) {
+    'ngInject';
+    this._AppConstants = AppConstants;
+    this._$http = $http;
+
+
+    this.current = null;
+  }
+
+  attemptAuth(type, credentials){
+    let route = (type === 'login') ? '/login' : '';
+    return this._$http({
+      url: this._AppConstants.api + '/users' + route,
+      method: 'POST',
+      data: {
+        user: credentials
+      }
+    }).then(
+      //On success....
+      (res) => {
+        //Store the users info for easy lookup
+        this.current = res.data.user;
+      }
+    );
+  }
+}
