40c2e52998db942ec023a4326ecf0cd5121a30d0
bestrandomnameever <bestrandomnameever@gmail.com>
2016-12-14 19:55:51 +0100

    Poging 2

--- gulpfile.js
+++ gulpfile.js
@@ -71,14 +71,14 @@ gulp.task('build', ['html', 'browserify'], function() {
 
 gulp.task('default', ['html', 'browserify'], function() {
 
-  browserSync.init(['./build/**/**.**'], {
-    server: "./build",
-    port: 4000,
-    notify: false,
-    ui: {
-      port: 4001
-    }
-  });
+  // browserSync.init(['./build/**/**.**'], {
+  //   server: "./build",
+  //   port: 4000,
+  //   notify: false,
+  //   ui: {
+  //     port: 4001
+  //   }
+  // });
 
   gulp.watch("src/index.html", ['html']);
   gulp.watch(viewFiles, ['views']);
